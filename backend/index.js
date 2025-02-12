24ad7a68a314e7f00fd1c38bb104d899b40ddd83
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-12 23:04:07 +0300

    index.js

---
+++ backend/index.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const app = express();
+app.get("/", (req, res) => res.send("Hello from the yohannes !"));
+app.listen(3000, () => console.log("Server started on port 3000"));
