e4df68515cc1c4249c36669a01b261e22327a5f3
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-14 20:21:55 +0300

    forgot pass

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {signup,login,logout,verifyEmail} from "../controllers/auth.controller.js";
+import {signup,login,logout,verifyEmail,forgotPassword} from "../controllers/auth.controller.js";
 const router = express.Router();
 router.post("/signup",signup)
 router.post("/login",login)
