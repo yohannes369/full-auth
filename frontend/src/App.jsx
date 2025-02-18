49129a98aa4282e2d722de86580839992953909b
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-18 12:59:51 +0300

    signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
-
+import {Route,Routes} from 'react-router-dom'
 import FloatingShape from "./components/FloatingShape"
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import Home from "./pages/Home";
 function App() {
   return (
     <div
@@ -9,9 +12,13 @@ function App() {
 <FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
 			<FloatingShape color='bg-emerald-500' size='w-48 h-48' top='70%' left='80%' delay={5} />
 			<FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
-
+     <Routes>
+      <Route path='/' element={Home} />
+      <Route path='/signup' element={<SignUpPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      </Routes>
     </div>
-   
+    
   )
 }
 export default App
