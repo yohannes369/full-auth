45da82dc315e90714d67552c53160e547e91d1c3
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-18 17:06:53 +0300

    auto storw

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotnev from "dotenv"; 
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./db/connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 dotnev.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(cors({origin:"http://localhost:5173",credentials:true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth",authRoutes);
