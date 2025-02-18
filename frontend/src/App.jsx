54148d42b2c9456c9c0d220a1c11bc15883b3c75
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-18 13:21:40 +0300

    ff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import {Route,Routes} from 'react-router-dom'
 import FloatingShape from "./components/FloatingShape"
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
+import EmailVerificationPage from "./pages/EmailVerificationPage";
 import Home from "./pages/Home";
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Route path='/' element={Home} />
       <Route path='/signup' element={<SignUpPage />} />
       <Route path='/login' element={<LoginPage />} />
+      <Route path='/verfy-emial' element={<EmailVerificationPage.jsx />} />
       </Routes>
     </div>
     
